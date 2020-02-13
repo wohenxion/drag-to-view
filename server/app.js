d9f6434ef0ecd2bfb558c7faf289ffc93a336618
webWang001 <984623233@qq.com>
2020-02-13 14:38:38 +0800

    修改配置文件

--- server/app.js
+++ server/app.js
@@ -14,19 +14,19 @@ app.use(statics(path.join(__dirname, publicPath)));
 // 使用ctx.body解析中间件
 app.use(bodyParser());
 render(app, {
-  root: path.join(__dirname, "view"),
-  layout: false,
-  viewExt: "html",
-  cache: false,
-  debug: false
+	root: path.join(__dirname, "view"),
+	layout: false,
+	viewExt: "html",
+	cache: false,
+	debug: false
 });
 app.use(function(ctx, next) {
-  ctx.state = ctx.state || {};
-  ctx.state.now = new Date();
-  ctx.state.ip = ctx.ip;
-  ctx.state.userID = "5dfadf3f2018fcfe2dfd3433";
-  ctx.state.version = "2.0.0";
-  return next();
+	ctx.state = ctx.state || {};
+	ctx.state.now = new Date();
+	ctx.state.ip = ctx.ip;
+	ctx.state.userID = "5dfadf3f2018fcfe2dfd3433";
+	ctx.state.version = "2.0.0";
+	return next();
 });
 
 // # mongodb 为协议
@@ -35,52 +35,53 @@ app.use(function(ctx, next) {
 // # localhost: 本地的地址（因为这是本地环境）
 // # 27017: mongodb的端口号(这个一般是默认值，也可以进行修改)
 // # test: 数据库的名字
-var db = "mongodb://admin:123456@localhost:27017/test";
+// var db = "mongodb://admin:123456@localhost:27017/test";
+var db = "mongodb://root:admin123..@localhost:27017/dragPage";
 
 // 连接
 mongoose.connect(
-  db,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  err => {
-    if (!err) {
-      // var schema = new mongoose.Schema({
-      //   userName: String,
-      //   age: Number,
-      //   sex: { type: String, default: "man" }
-      // });
-      // var user = mongoose.model("user", schema);
-      // var data = {
-      //   age: 12,
-      //   sex: "woman"
-      // };
-      // var _id = "5dfadf3f2018fcfe2dfd3434";
-      // user.updateOne(
-      //   { _id },
-      //   {
-      //     ...data
-      //   },
-      //   function(err, docs) {
-      //     console.log(docs);
-      //   }
-      // );
-      console.log("succese");
-    }
-  }
+	db,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},
+	err => {
+		if (!err) {
+			// var schema = new mongoose.Schema({
+			//   userName: String,
+			//   age: Number,
+			//   sex: { type: String, default: "man" }
+			// });
+			// var user = mongoose.model("user", schema);
+			// var data = {
+			//   age: 12,
+			//   sex: "woman"
+			// };
+			// var _id = "5dfadf3f2018fcfe2dfd3434";
+			// user.updateOne(
+			//   { _id },
+			//   {
+			//     ...data
+			//   },
+			//   function(err, docs) {
+			//     console.log(docs);
+			//   }
+			// );
+			console.log("succese");
+		}
+	}
 );
 
 const getData = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, "/test.json"), (err, data) => {
-      if (err) {
-        reject(err);
-        return console.error(err);
-      }
-      resolve(JSON.parse(data.toString()));
-    });
-  });
+	return new Promise((resolve, reject) => {
+		fs.readFile(path.join(__dirname, "/test.json"), (err, data) => {
+			if (err) {
+				reject(err);
+				return console.error(err);
+			}
+			resolve(JSON.parse(data.toString()));
+		});
+	});
 };
 
 // app.use(async ctx => {
@@ -93,20 +94,20 @@ const getData = () => {
 
 // 配置路由
 fs.readdirSync(path.join(__dirname, "./routes")).forEach(route => {
-  let api = require(`./routes/${route}`);
-  router.use(
-    `/${route.replace(".js", "")}`,
-    api.routes(),
-    api.allowedMethods()
-  );
+	let api = require(`./routes/${route}`);
+	router.use(
+		`/${route.replace(".js", "")}`,
+		api.routes(),
+		api.allowedMethods()
+	);
 });
 
 router.get("/", async ctx => {
-  let pageData;
-  pageData = await getData();
-  await ctx.render("index", {
-    pageData
-  });
+	let pageData;
+	pageData = await getData();
+	await ctx.render("index", {
+		pageData
+	});
 });
 // 加载路由中间件
 app.use(router.routes()).use(router.allowedMethods());
